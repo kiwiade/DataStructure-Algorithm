f26e24aee1cb998084a9fd5ebdc0c7c89c54cd1a
kiwiade <bluestar373@naver.com>
2019-05-15 18:12:46 +0900

    [Add] Algorithm / Project Create

---
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "Sort.h"
+
+int main()
+{
+	const int maxSize = 100;
+
+	int* datas = MakeRandomNumber(maxSize);
+	Print(datas, maxSize);
+	std::cout << std::endl;
+
+	//SelectionSort(datas, maxSize);
+	//InsertionSort(datas, maxSize);
+	//BubbleSort(datas, maxSize);
+	//ShellSort(datas, maxSize);
+	QuickSort(datas, 0, maxSize-1);
+	Print(datas, maxSize);
+}
